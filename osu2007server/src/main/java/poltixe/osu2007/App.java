61326aa434aa7345ec36267a85e439bee889aa5d
PoltixeTheDerg <34501060+PoltixeTheDerg@users.noreply.github.com>
2020-09-21 01:17:44 -0700

    Basic maven project setup

---
+++ osu2007server/src/main/java/poltixe/osu2007/App.java
@@ -0,0 +1,13 @@
+package poltixe.osu2007;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ osu2007server/src/test/java/poltixe/osu2007/AppTest.java
@@ -0,0 +1,20 @@
+package poltixe.osu2007;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
