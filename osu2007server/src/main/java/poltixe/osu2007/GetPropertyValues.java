1386873578f38d72209d790d873b6e2cee9d6f02
PoltixeTheDerg <34501060+PoltixeTheDerg@users.noreply.github.com>
2020-09-29 14:36:30 -0700

    Fix options file

--- osu2007server/src/main/java/poltixe/osu2007/GetPropertyValues.java
+++ osu2007server/src/main/java/poltixe/osu2007/GetPropertyValues.java
@@ -1,9 +1,6 @@
 package poltixe.osu2007;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Date;
+import java.io.*;
 import java.util.Properties;
 
 public class GetPropertyValues {
@@ -15,12 +12,12 @@ public class GetPropertyValues {
             Properties prop = new Properties();
             String propFileName = "config.properties";
 
-            inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+            inputStream = new FileInputStream(propFileName);
 
             if (inputStream != null) {
                 prop.load(inputStream);
             } else {
-                throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+                throw new FileNotFoundException("property file '" + propFileName + "' not found");
             }
 
             App.mySqlServer = prop.getProperty("mysqlserver");
